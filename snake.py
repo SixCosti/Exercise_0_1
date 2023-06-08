c0225dcb87cd712252b7fcad43cf114e1a770ea1
Costi <costi@qa.com>
2023-06-08 11:42:26 +0100

    Added a python file from my local copy

---
+++ snake.py
@@ -0,0 +1,8 @@
+import sys
+
+print("Neo, They've found you. Follow the white rabbit...")
+name = input()
+if name == ("Neo"):
+    print("Where is my spatula")
+else:
+    print("You're not the chosen one")
